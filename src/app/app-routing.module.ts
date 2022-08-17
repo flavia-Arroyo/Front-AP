329d070f04e7fb546200ce3f9629027679564961
Flavia Arroyo <flavia.e.arroyo@gmail.com>
2022-08-17 20:09:03 -0300

    ajustes menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+//definicion rutas
+const routes: Routes = [
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
